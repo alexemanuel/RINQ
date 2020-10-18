bb6fee067dfccc101e2e18f0220cf0f7d99c4328
Alex Emanuel <alexemanuel>
2020-10-17 22:47:54 -0300

    absenceManage() method implementation

--- src/main/java/com/rinq/controllers/FaltasController.java
+++ src/main/java/com/rinq/controllers/FaltasController.java
@@ -7,17 +7,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.rinq.models.Aula;
 import com.rinq.models.Curso;
 import com.rinq.models.Discente;
+import com.rinq.models.Disciplina;
 import com.rinq.models.Docente;
+import com.rinq.models.Falta;
+import com.rinq.models.DTO.AulaDTO;
+import com.rinq.models.DTO.FaltaDTO;
+import com.rinq.repositories.AulaRepository;
 import com.rinq.repositories.DiscenteRepository;
+import com.rinq.repositories.DisciplinaRepository;
 import com.rinq.repositories.DocenteRepository;
+import com.rinq.repositories.FaltaRepository;
 import com.rinq.repositories.UsuarioRepository;
 
 
 @Controller
-public class AbsenceController {
+public class FaltasController {
 
 	@Autowired
 	UsuarioRepository usuarioRepository;
@@ -25,18 +34,49 @@ public class AbsenceController {
 	DocenteRepository docenteRepository;
 	@Autowired
 	DiscenteRepository discenteRepository;
+	@Autowired
+	DisciplinaRepository disciplinaRepository;
+	@Autowired
+	AulaRepository aulaRepository;
+	@Autowired
+	FaltaRepository faltaRepository;
 	
 	@GetMapping("/chamada")
-	public String absenceVision(Model model, Principal principal) {
+	public String showDiscenteList(Model model, Principal principal) {
 		String cpf = principal.getName();
-		
+			
 		if(docenteRepository.existsByCpf(cpf)){
 			Docente docente = docenteRepository.findByCpf(cpf);
-			Curso course = docente.getCurso();
+			Curso curso = docente.getCurso();	
+			Disciplina disciplina = disciplinaRepository.findByNome(docente.getDisciplina());
 			
-			List<Discente> students = discenteRepository.findByCursoOrderByNome(course);	
-			model.addAttribute("students", students);
+			Aula aula = new Aula();
+			aula.setDisciplina(disciplina);
+			aula.setDocente(docente);
+			
+			List<Discente> discentes = discenteRepository.findByCursoOrderByNome(curso);
+			
+			AulaDTO aulaDTO = new AulaDTO(aula);			
+			FaltaDTO faltaDTO = new FaltaDTO(discentes);
+			
+			model.addAttribute("discentes", discentes);
+			model.addAttribute("faltasDTO", faltaDTO);
+			model.addAttribute("aulaDTO", aulaDTO);
 		}
 		return "chamada";
+	}
+	
+	@PostMapping("/chamada")
+	public String absenceManage(AulaDTO aulaDTO, FaltaDTO faltaDTO) {
+		List<Falta> faltas = faltaDTO.getFaltas();
+		Aula aula = aulaDTO.getAula();
+				
+		faltas.forEach(falta -> falta.setAula(aula));
+		aula.setFaltas(faltas);
+		
+		aulaRepository.save(aula);
+		faltaRepository.saveAll(faltas);
+		
+		return "redirect:/home";
 	}
 }
